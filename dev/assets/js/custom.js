d9c8fabe7f0aa522b397a082924202afe672b1a7
margemarg <manukianmr@gmail.com>
2024-06-08 22:31:33 +0300

    header desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,8 +1,17 @@
-  $(document).ready(function() {
-    $('.single-item').slick();
+$(document).ready(function() {
+  $('.single-item').slick();
 
-    Fancybox.bind("[data-fancybox]", {
-      // Your custom options
-    });
+  Fancybox.bind("[data-fancybox]", {
+    // Your custom options
   });
+});
 
+const burger = document.querySelector('#burger');
+const mobileContainer = document.querySelector('.mobile-container');
+console.log(mobileContainer)
+
+function showBurgerMenu() {
+  mobileContainer.classList.toggle('show')
+}
+
+burger.addEventListener('click', showBurgerMenu)
