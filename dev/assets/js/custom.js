a9d63ab99258a2f7d74ee322d55fa7295772385b
margemarg <manukianmr@gmail.com>
2024-06-16 01:44:34 +0300

    first screen mobile done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -6,11 +6,13 @@ Fancybox.bind("[data-fancybox]", {
 
 //Burger_Menu
 const burger = document.querySelector('#burger');
+const body = document.querySelector('body');
 const mobileContainer = document.querySelector('.mobile-container');
 
 function showBurgerMenu() {
   mobileContainer.classList.toggle('show')
   burger.classList.toggle('is-active')
+  body.classList.toggle('overflow-hidden')
 }
 
 burger.addEventListener('click', showBurgerMenu)
@@ -18,19 +20,28 @@ burger.addEventListener('click', showBurgerMenu)
 
 //Swiper
 const swiper = new Swiper('.slider-info', {
-  spaceBetween: 40,
+  spaceBetween: 15,
   loop: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    enabled: false,
   },
   autoplay: {
     delay: 5000,
   },
+  breakpoints: {
+    992: {
+      spaceBetween: 40,
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    }
+  }
 });
 
 
